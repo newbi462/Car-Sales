608d4f456df5e9fcfd7ea68b94eb2a6cb21b3771
newbi462 <isthisemailfree@gmail.com>
2019-12-11 15:16:00 -0500

    Set up "empty" reducer and initial state 👉 Set up store and Provider 👉

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+import { carReducer } from "./reducers/carReducer";
+
 const App = () => {
   const state = {
     additionalPrice: 0,
