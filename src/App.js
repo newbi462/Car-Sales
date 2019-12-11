e348a0eeb8cedf0bfad809456d9a4247342ce1bf
newbi462 <isthisemailfree@gmail.com>
2019-12-11 15:36:13 -0500

    Connect components 👉 Add events and event handlers in UI 👉 Build action creators 👉

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-import { carReducer } from "./reducers/carReducer";
+import { connect } from "react-redux";
 
-const App = () => {
-  const state = {
+import { carReducer } from "./reducers/carReducer";
+import { removeFeature, buyItem } from "./actions/appActions";
+
+const App = (props) => {
+/*  const state = {
     additionalPrice: 0,
     car: {
       price: 26395,
@@ -23,28 +26,42 @@ const App = () => {
       { id: 3, name: 'Premium sound system', price: 500 },
       { id: 4, name: 'Rear spoiler', price: 250 }
     ]
-  };
+  };*/
 
-  const removeFeature = item => {
+/*  const removeFeature = item => {
     // dispatch an action here to remove an item
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
-  };
+  };*/
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+//export default App;
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+  }; // thesese are the props, like with Formik must manulay list
+};
+
+export default connect(
+  mapStateToProps,
+  { removeFeature, buyItem }
+)(App);
---
+++ src/actions/appActions.js
@@ -0,0 +1,9 @@
+export const removeFeature = item => {
+  // dispatch an action here to remove an item
+};
+
+export const buyItem = item => {
+  // dipsatch an action here to add an item
+};
+
+// NOt sure how else to get in the connet???
